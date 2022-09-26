381179e9bb95db369753b57e621edea1eb4b3856
kapil11Agnihotri <114352438+kapil11Agnihotri@users.noreply.github.com>
2022-09-26 11:46:42 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log('i love git')
+
+var headerTitle=document.getElementById('header-title');
+//console.log(headerTitle);
+//headerTitle.innerHTML='<h3>hello</h3>';
+headerTitle.style.borderBottom='solid 3px #000';
+
+var items=document.querySelector('.title');
+items.style.fontWeight='bold';
+items.style.color='green'; 
