624d5ce112e3fa200583febf644abf9d434fad79
kapil11Agnihotri <kapil90444@gmail.com>
2022-09-26 21:50:00 +0530

    first commit

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-console.log('i love git')
+
+var headerTitle=document.getElementById('header-title');
+//console.log(headerTitle);
+//headerTitle.innerHTML='<h3>hello</h3>';
+headerTitle.style.borderBottom='solid 3px #000';
