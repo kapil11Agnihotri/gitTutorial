11b0322f14d9622be1f388bab4b7d26079722ba5
kapil11Agnihotri <kapil90444@gmail.com>
2022-09-27 13:36:00 +0530

    keys feature added

--- My HTML/basics js/appointment.js
+++ My HTML/basics js/appointment.js
@@ -18,12 +18,30 @@ function onSubmit(e) {
     // Remove error after 3 seconds
     setTimeout(() => msg.remove(), 3000);
   } else {
+    const li = document.createElement('li');
+
+    // Add text node with input values
+    li.appendChild(document.createTextNode(`${nameInput.value}: ${emailInput.value}`));
+
+    // Add HTML
+    // li.innerHTML = `<strong>${nameInput.value}</strong>e: ${emailInput.value}`;
+
+    // Append to ul
+    userList.appendChild(li);
+
     let myObj={
        name:nameInput.value,
        email:emailInput.value
     }
     let myObj_serialized=JSON.stringify(myObj);
-    localStorage.setItem('myObj',myObj_serialized);
+
+    localStorage.setItem(nameInput.value,myObj_serialized);
+    console.log(Object.keys(myObj));
+    const keysArr=Object.keys(myObj);
+    keysArr.forEach(key =>{
+        console.log(myObj[key])
+    });
+    
     // Clear fields
     nameInput.value = '';
     emailInput.value = '';
