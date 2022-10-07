4675c3120522c0074379d775f2f843ce08cf1cac
kapil11Agnihotri <kapil90444@gmail.com>
2022-10-07 20:33:51 +0530

    nnnnn

--- Project/project.js
+++ Project/project.js
@@ -1,60 +1,115 @@
-const myForm = document.querySelector('#myForm');
-const amountInput=document.getElementById('amount')
-const discriptionInput=document.getElementById('discription')
-const catagoryInput=document.getElementById('catagory')
-const expenceList=document.querySelector('.list')
+const myForm = document.querySelector("#myForm");
+const amountInput = document.getElementById("amount");
+const discriptionInput = document.getElementById("discription");
+const catagoryInput = document.getElementById("catagory");
+const expenceList = document.querySelector(".expence");
+const expenceId = document.querySelector(".expence");
 
 //add event listner
 
-myForm.addEventListener('submit',onSubmit);
+myForm.addEventListener("submit", onSubmit);
 
-function onSubmit(e){
-    e.preventDefault()
-   const li=document.createElement('li');
-   
-   //add text nodes with input values 
+function onSubmit(e) {
+  e.preventDefault();
 
-   li.appendChild(document.createTextNode(`${amountInput.value} - ${catagoryInput.value} - ${discriptionInput.value}`));
-  
-   
-   
-   // add html
-  
-   li.innerHTML=`${amountInput.value} - ${catagoryInput.value} - ${discriptionInput.value}`
-   //add buttons
-   const editBtn=document.createElement('button')
-   const deleteBtn=document.createElement('button')
-   // add class to buttons
-   editBtn.className='edit'
-   deleteBtn.className='delete' 
+  const li = document.createElement("li");
+
+  //add text nodes with input values
+
+  li.appendChild(
+    document.createTextNode(
+      `${amountInput.value} - ${catagoryInput.value} - ${discriptionInput.value}`
+    )
+  );
+
+  // add html
+
+  li.innerHTML = `${amountInput.value} - ${catagoryInput.value} - ${discriptionInput.value}`;
+  //add buttons
+  const editBtn = document.createElement("button");
+  const deleteBtn = document.createElement("button");
+  // add class to buttons
+  editBtn.className = "edit";
+  deleteBtn.className = "delete";
 
   // append textnode
-  editBtn.appendChild(document.createTextNode('Edit'))
-  deleteBtn.appendChild(document.createTextNode('Delete'))
-   //add buttons to li
-   li.appendChild(editBtn)
-   li.appendChild(deleteBtn)
+  editBtn.appendChild(document.createTextNode("Edit"));
+  deleteBtn.appendChild(document.createTextNode("Delete"));
+  //add buttons to li
+  li.appendChild(editBtn);
+  li.appendChild(deleteBtn);
 
-    expenceList.appendChild(li);
-// add to local storage
-let myObj={
+  expenceList.appendChild(li);
+  // add to local storage
+  let myObj = {
+    amount: amountInput.value,
+    discription: discriptionInput.value,
+    catgory: catagoryInput.value,
+  };
 
-    amount:amountInput.value,
-    discription:discriptionInput.value,
-    catgory:catagoryInput.value
+  console.log(Object.keys(myObj));
 
- }
+  const keysArr = Object.keys(myObj);
 
- console.log(Object.keys(myObj));
+  keysArr.forEach((key) => {
+    console.log(myObj[key]);
+  });
 
- const keysArr=Object.keys(myObj);
+  axios
+    .post(
+      "https://crudcrud.com/api/c0ab1c796ff446ab8010f3148e015ff8/expence",
+      myObj
+    )
+    .then((response) => console.log(response))
+    .catch((error) => console.log(error));
+}
 
- keysArr.forEach(key =>{
+window.addEventListener("DOMContentLoaded", () => {
+  axios
+    .get("https://crudcrud.com/api/c0ab1c796ff446ab8010f3148e015ff8/expence")
+    .then((responce) => {
+      console.log(responce);
 
-     console.log(myObj[key])
+      for (let i = 0; i < responce.data.length; i++) {
+        showNewExpence(responce.data[i]);
+      }
+    })
+    .catch((error) => console.log(error));
+});
 
- });
+function showNewExpence(expence) {
+  const parentNode = document.getElementsByClassName("expence");
+  const childHTML = `<li id="${expence._id}">${expence.amount}-${expence.catagory}-${expence.discription}
+ <button onclick="editUser('${expence.amount}','${expence.catagory}','${expence.discription}','${expence._id}')">edit</button>
+ <button onclick="deleteUser('${expence._id}')">delete</button>
+ </li>`;
 
+  parentNode.innerHTML += childHTML;
+  console.log(expence)
+}
+function editUser(amount, discription, catagory, expenceId) {
+  document.getElementById("amount").value = amount;
+  document.getElementById("discription").value = discription;
+  document.getElementById("catagory").value = catagory;
 
+  deleteUser(expenceId);
+}
 
+function deleteUser(expenceId) {
+  axios
+    .delete(
+      `https://crudcrud.com/api/c0ab1c796ff446ab8010f3148e015ff8/expence/${expenceId}`
+    )
+    .then((responce) => {
+      removeExpence(expenceId);
+    })
+    .catch((error) => console.log(error));
+}
+
+function removeExpence(expenceId) {
+  const parentNode = document.getElementsByClassName("expence");
+  const childtobeDeleted = document.getElementById(expenceId);
+  if (childtobeDeleted) {
+    parentNode.removeChild(childtobeDeleted);
+  }
 }
